1e56e6f6c0e2d7a1b42d28c9a3970a97629107bc
Rene van der Ark <r.van.der.ark@gmail.com>
2017-03-07 16:48:25 +0100

    removes some boilerplate.

--- src/app.js
+++ src/app.js
@@ -83,8 +83,6 @@ app.post(`${PATH_PREFIX}/webhook`, function (req, res) {
     // Iterate over each entry
     // There may be multiple if batched
     data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
 
       // Iterate over each messaging event
       pageEntry.messaging.forEach(function(messagingEvent) {
@@ -196,6 +194,34 @@ function receivedAuthentication(event) {
   sendTextMessage(senderID, "Authentication successful");
 }
 
+
+function handleSearchResponse(senderID) {
+    return function (err, data) {
+        sendTypingOff(senderID);
+
+        if (err) {
+            sendTextMessage(senderID, err);
+        } else {
+            console.log(data);
+            if (data.type === 'buttons') {
+                sendButtonMessage(senderID, data.buttons);
+            }
+
+            if (data.type === 'images') {
+                sendTextMessage(senderID, `Je gaat zo zien: ${data.images.label}, ${data.images.description}`);
+                sendImageMessage(senderID, data.images.image);
+                setTimeout(function () {
+                    sendURL(senderID, `http://www.wikidata.org/wiki/${data.images.id}`);
+                }, 3000);
+            }
+
+            if (data.type === 'text') {
+                sendTextMessage(senderID, data.text);
+            }
+        }
+    };
+}
+
 /*
  * Message Event
  *
@@ -220,22 +246,8 @@ function receivedMessage(event) {
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
-  var isEcho = message.is_echo;
-  var messageId = message.mid;
-  var appId = message.app_id;
-  var metadata = message.metadata;
-
   // You may get a text or attachment but not both
   var messageText = message.text;
-  var messageAttachments = message.attachments;
-  var quickReply = message.quick_reply;
-
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s",
-      messageId, appId, metadata);
-    return;
-  }
 
     // Currently the only type we support is text
     if (messageText) {
@@ -243,127 +255,23 @@ function receivedMessage(event) {
         sendTextMessage(senderID, "Ik ben nu aan het zoeken, een momentje...");
         sendTypingOn(senderID);
 
-        //noinspection JSAnnotator
-        function handle(err, data) {
-            sendTypingOff(senderID);
-
-            if (err) {
-                sendTextMessage(senderID, err);
-            } else {
-                console.log(data);
-                if (data.type === 'buttons') {
-                    sendButtonMessage(senderID, data.buttons);
-                }
-
-                if (data.type === 'images') {
-                  sendTextMessage(senderID, `Je gaat zo zien: ${data.images.label}, ${data.images.description}`);
-                  sendImageMessage(senderID, data.images.image);
-                  setTimeout(function() {
-                    sendURL(senderID, `http://www.wikidata.org/wiki/${data.images.id}`);
-                  }, 3000);
-                }
-
-                if (data.type === 'text') {
-                    sendTextMessage(senderID, data.text);
-                }
-            }
-        }
 
         if (messageText.indexOf('-') !== -1) {
           var dates = messageText.split('-');
 
-          bot.painterByDate(dates[1], dates[0], handle);
+          bot.painterByDate(dates[1], dates[0], handleSearchResponse(senderID));
         } else if (messageText === 'utrecht') {
-          bot.getMonuments(handle);
+          bot.getMonuments(handleSearchResponse(senderID));
         } else if (messageText === 'surprise') {
-          bot.randomArtist(handle);
+          bot.randomArtist(handleSearchResponse(senderID));
         } else {
-          bot.searchPainters(messageText, handle);
+          bot.searchPainters(messageText, handleSearchResponse(senderID));
         }
     } else {
         sendTextMessage(senderID, "Sorry, dit snap ik even niet.");
     }
 
     return;
-
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s",
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
-
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
-  }
-
-  if (messageText) {
-
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
-    switch (messageText) {
-      case 'image':
-        sendImageMessage(senderID);
-        break;
-
-      case 'gif':
-        sendGifMessage(senderID);
-        break;
-
-      case 'audio':
-        sendAudioMessage(senderID);
-        break;
-
-      case 'video':
-        sendVideoMessage(senderID);
-        break;
-
-      case 'file':
-        sendFileMessage(senderID);
-        break;
-
-      case 'button':
-        sendButtonMessage(senderID);
-        break;
-
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      case 'receipt':
-        sendReceiptMessage(senderID);
-        break;
-
-      case 'quick reply':
-        sendQuickReply(senderID);
-        break;
-
-      case 'read receipt':
-        sendReadReceipt(senderID);
-        break;
-
-      case 'typing on':
-        sendTypingOn(senderID);
-        break;
-
-      case 'typing off':
-        sendTypingOff(senderID);
-        break;
-
-      case 'account linking':
-        sendAccountLinking(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 
@@ -529,94 +437,6 @@ function sendImageMessage(recipientId, url) {
 }
 
 /*
- * Send a Gif using the Send API.
- *
- */
-function sendGifMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "image",
-        payload: {
-          url: SERVER_URL + "/assets/instagram_logo.gif"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send audio using the Send API.
- *
- */
-function sendAudioMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "audio",
-        payload: {
-          url: SERVER_URL + "/assets/sample.mp3"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send a video using the Send API.
- *
- */
-function sendVideoMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "video",
-        payload: {
-          url: SERVER_URL + "/assets/allofus480.mov"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send a file using the Send API.
- *
- */
-function sendFileMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "file",
-        payload: {
-          url: SERVER_URL + "/assets/test.txt"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
  * Send a text message using the Send API.
  *
  */
@@ -662,36 +482,6 @@ function sendButtonMessage(recipientId, buttons) {
     };
 
     callSendAPI(data);
-
-  // var messageData = {
-  //   recipient: {
-  //     id: recipientId
-  //   },
-  //   message: {
-  //     attachment: {
-  //       type: "template",
-  //       payload: {
-  //         template_type: "button",
-  //         text: "This is test text",
-  //         buttons:[{
-  //           type: "web_url",
-  //           url: "https://www.oculus.com/en-us/rift/",
-  //           title: "Open Web URL"
-  //         }, {
-  //           type: "postback",
-  //           title: "Trigger Postback",
-  //           payload: "DEVELOPER_DEFINED_PAYLOAD"
-  //         }, {
-  //           type: "phone_number",
-  //           title: "Call Phone Number",
-  //           payload: "+16505551234"
-  //         }]
-  //       }
-  //     }
-  //   }
-  // };
-
-  // callSendAPI(messageData);
 }
 
 function sendURL(recId, url) {
@@ -716,173 +506,6 @@ function sendURL(recId, url) {
     })
 }
 
-/*
- * Send a Structured Message (Generic Message type) using the Send API.
- *
- */
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: SERVER_URL + "/assets/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: SERVER_URL + "/assets/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send a receipt message using the Send API.
- *
- */
-function sendReceiptMessage(recipientId) {
-  // Generate a random receipt ID as the API requires a unique ID
-  var receiptId = "order" + Math.floor(Math.random()*1000);
-
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message:{
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "receipt",
-          recipient_name: "Peter Chang",
-          order_number: receiptId,
-          currency: "USD",
-          payment_method: "Visa 1234",
-          timestamp: "1428444852",
-          elements: [{
-            title: "Oculus Rift",
-            subtitle: "Includes: headset, sensor, remote",
-            quantity: 1,
-            price: 599.00,
-            currency: "USD",
-            image_url: SERVER_URL + "/assets/riftsq.png"
-          }, {
-            title: "Samsung Gear VR",
-            subtitle: "Frost White",
-            quantity: 1,
-            price: 99.99,
-            currency: "USD",
-            image_url: SERVER_URL + "/assets/gearvrsq.png"
-          }],
-          address: {
-            street_1: "1 Hacker Way",
-            street_2: "",
-            city: "Menlo Park",
-            postal_code: "94025",
-            state: "CA",
-            country: "US"
-          },
-          summary: {
-            subtotal: 698.99,
-            shipping_cost: 20.00,
-            total_tax: 57.67,
-            total_cost: 626.66
-          },
-          adjustments: [{
-            name: "New Customer Discount",
-            amount: -50
-          }, {
-            name: "$100 Off Coupon",
-            amount: -100
-          }]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send a message with Quick Reply buttons.
- *
- */
-function sendQuickReply(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: "What's your favorite movie genre?",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"Action",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"Comedy",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"Drama",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-        }
-      ]
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
- * Send a read receipt to indicate the message has been read
- *
- */
-function sendReadReceipt(recipientId) {
-  console.log("Sending a read receipt to mark message as seen");
-
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    sender_action: "mark_seen"
-  };
-
-  callSendAPI(messageData);
-}
 
 /*
  * Turn typing indicator on
@@ -919,38 +542,16 @@ function sendTypingOff(recipientId) {
 }
 
 /*
- * Send a message with the account linking call-to-action
- *
- */
-function sendAccountLinking(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: "Welcome. Link your account.",
-          buttons:[{
-            type: "account_link",
-            url: SERVER_URL + "/authorize"
-          }]
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-/*
  * Call the Send API. The message data goes in the body. If successful, we'll
  * get the message id in a response
  *
  */
 function callSendAPI(messageData) {
+  if (process.env.MODE === 'mock') {
+    console.log(JSON.stringify(messageData, null, 2));
+    return;
+  }
+
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: PAGE_ACCESS_TOKEN },
